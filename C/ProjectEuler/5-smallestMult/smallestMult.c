8a737ac68e3ba38291f25fe3aef001f4d9a95eb6
John <john.wilkins@colorado.edu>
2018-10-12 20:25:04 -0600

    Completed PE #5

--- C/ProjectEuler/5-smallestMult/smallestMult.c
+++ C/ProjectEuler/5-smallestMult/smallestMult.c
@@ -8,11 +8,27 @@
  *  from 1 to 20?
 */
 
-#include <stdlib.h>
 #include <stdio.h>
+
+typedef int bool;
+#define true 1
+#define false 0
 
 
 int main() {
+    int num = 2520;
+    bool notDivisible = true;
 
+    while(notDivisible) {
+        for(int i = 2; i <= 20; i++) {
+            if(num % i == 0) {
+                if(i == 20) {
+                    notDivisible = false;
+                    break; }
+                continue; }
+            else {
+                num++;
+                break; }}}
 
+    printf("Num = %d\n", num);
     return 0; }
