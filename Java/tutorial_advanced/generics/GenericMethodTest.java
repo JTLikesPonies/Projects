408ad33b2c740b386fa8f91bafd80e7016c341c1
John <john@cataworx.com>
2020-03-23 07:33:07 -0600

    Initial commit

---
+++ Java/tutorial_advanced/generics/GenericMethodTest.java
@@ -0,0 +1,27 @@
+public class GenericMethodTest {	
+	// Generic method printArray
+	public static < E > void printArray( E[] inputArray ) {
+		// Display array elements
+		for( E element: inputArray ) {
+			System.out.printf( "%s ", element );
+		}
+		System.out.println();
+	}
+
+	public static void main( String[] args ) {
+		// Create arrays of Integer, Double and Character
+		Integer[] intArray = { 1, 2, 3, 4, 5 };
+		Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+		Character[] charArray = { 'H', 'E', 'L', 'L', 'O' } ;
+
+		System.out.println( "Array integerArray contains: " );
+		printArray( intArray );		// pass an Int array
+
+		System.out.println( "\nArray doubleArray contains: " );
+		printArray( doubleArray );	// pass a Double array
+
+		System.out.println( "\nArray charactersArray contains: " );
+		printArray( charArray );	// pass a Char array
+	}
+		
+}
